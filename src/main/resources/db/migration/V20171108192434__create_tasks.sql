5dce72008962681eeffc1f34280ce27b6bee31cd
Scott Pullen <s.pullen05@gmail.com>
2017-11-09 09:11:38 -0500

    Changing some stuffz

--- src/main/resources/db/migration/V20171108192434__create_tasks.sql
+++ src/main/resources/db/migration/V20171108192434__create_tasks.sql
@@ -3,7 +3,7 @@ CREATE TABLE users (
   user_id UUID REFERENCES users(id) NOT NULL,
   content TEXT,
   status TEXT NOT NULL,
-  due_by DATE,
+  due_date timestamp without time zone,
   created_at timestamp without time zone,
   updated_at timestamp without time zone
 );
