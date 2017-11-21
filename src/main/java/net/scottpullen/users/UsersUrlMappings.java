30d121fb1aa9b64052e1d28c74d01d271541ee76
Scott Pullen <s.pullen05@gmail.com>
2017-11-21 07:36:31 -0500

    Adding UsersUrlMappings

---
+++ src/main/java/net/scottpullen/users/UsersUrlMappings.java
@@ -0,0 +1,12 @@
+package net.scottpullen.users;
+
+import ratpack.func.Action;
+import ratpack.handling.Chain;
+
+public class UsersUrlMappings implements Action<Chain> {
+
+    @Override
+    public void execute(Chain chain) {
+
+    }
+}
