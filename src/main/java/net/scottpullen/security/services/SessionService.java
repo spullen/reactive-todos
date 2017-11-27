3a9238c98417222db4acb6d595d30007f11207c8
Scott Pullen <s.pullen05@gmail.com>
2017-11-27 17:09:38 -0500

    Filling in the rest of the session service

--- src/main/java/net/scottpullen/security/services/SessionService.java
+++ src/main/java/net/scottpullen/security/services/SessionService.java
@@ -1,8 +1,11 @@
 package net.scottpullen.security.services;
 
+import com.lambdaworks.crypto.SCryptUtil;
 import io.reactivex.Single;
+import net.scottpullen.common.exceptions.NotFoundException;
 import net.scottpullen.security.commands.SessionCommand;
 import net.scottpullen.security.entities.AuthenticationToken;
+import net.scottpullen.users.entities.User;
 import net.scottpullen.users.repositories.UserRepository;
 
 import java.util.Optional;
@@ -18,9 +21,31 @@ public class SessionService {
     }
 
     public Single<Optional<AuthenticationToken>> perform(SessionCommand command) {
-        return Single.create(subscriber -> {
+        return userRepository.findByEmail(command.getEmail())
+            .flatMap(this::verifyUser)
+            .flatMap((user) -> verifyPassword(command, user));
+    }
 
+
+    private Single<User> verifyUser(Optional<User> maybeUser) {
+        return Single.create(subscriber -> {
+            if(maybeUser.isPresent()) {
+                subscriber.onSuccess(maybeUser.get());
+            } else {
+                subscriber.onError(new NotFoundException("Failed to find user for given email."));
+            }
         });
     }
 
+    private Single<Optional<AuthenticationToken>> verifyPassword(SessionCommand command, User user) {
+        return Single.create(subscriber -> {
+            boolean validPassword = SCryptUtil.check(command.getPassword(), user.getPasswordDigest());
+
+            if(validPassword) {
+                subscriber.onSuccess(tokenGeneratorService.perform(user.getId()));
+            } else {
+                subscriber.onSuccess(Optional.empty());
+            }
+        });
+    }
 }
