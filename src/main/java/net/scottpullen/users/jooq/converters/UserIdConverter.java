83edf40f57af49fcc1ba6734f98d8c6b51a5465f
Scott Pullen <s.pullen05@gmail.com>
2017-11-28 16:49:58 -0500

    using new converter/binding for UserId

--- src/main/java/net/scottpullen/users/jooq/converters/UserIdConverter.java
+++ src/main/java/net/scottpullen/users/jooq/converters/UserIdConverter.java
@@ -1,32 +1,10 @@
 package net.scottpullen.users.jooq.converters;
 
+import net.scottpullen.common.jooq.converters.EntityIdConverter;
 import net.scottpullen.users.entities.UserId;
-import org.jooq.Converter;
 
-import java.util.UUID;
-
-public class UserIdConverter implements Converter<Object, UserId> {
-    @Override
-    public UserId from(Object t) {
-        if(t == null) {
-            return null;
-        }
-
-        return new UserId(t + "");
-    }
-
-    @Override
-    public UUID to(UserId u) {
-        return u == null ? null : u.getValue();
-    }
-
-    @Override
-    public Class<Object> fromType() {
-        return Object.class;
-    }
-
-    @Override
-    public Class<UserId> toType() {
-        return UserId.class;
+public class UserIdConverter extends EntityIdConverter<UserId> {
+    public UserIdConverter() {
+        super(UserId.class);
     }
 }
